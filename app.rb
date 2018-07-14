53ba29090710e7ccfccc4dce1e5fd282c773ed0d
ERIC KELLY <eakelly220@gmail.com>
2018-07-14 20:08:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,14 +7,13 @@ end
 class App < Sinatra::Base
 
   get '/' do
+    @session = session
     erb :index
   end
 
   post '/checkout' do
 
     @item = Item.new(params[:item])
-    session["item"] = @item
-    @session = session
     erb :checkout
   end
 
